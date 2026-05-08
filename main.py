5ad5cd9b3c81ec0f342d0429d7785d9e3bd4672a
Elias S <elias.sangberg@skola.taby.se>
2026-05-08 12:16:14 +0200

    gjorde ett öga

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
 import turtle
-fönster = turtle.Screen()
+
+
+
+iris = input("Vad är din primära ögonfärg?")
+iris2 = input("Vad är din sekundära ögonfärg?")
+
+screen = turtle.Screen()
+screen.bgcolor("#000000")
+
+pupill = "black"
+
+lob = "white"
+
+vinkel = 2
 
 min_skoldpadda = turtle.Turtle()
 
 min_skoldpadda.shape("turtle")
 
-min_skoldpadda.color("green")
+min_skoldpadda.speed(0)
 
-min_skoldpadda.speed(7)
+def rita_lob(radie):
+        min_skoldpadda.color(lob)
+        min_skoldpadda.begin_fill()
+        min_skoldpadda.circle(radie)
+        min_skoldpadda.end_fill()
 
-antal = int(input("Hur många sidor ska din figur ha? "))
-storlek = int(input("Hur stor ska den vara?"))
 
-vinkel = 360 / antal
+def rita_oga(radie):
+        min_skoldpadda.color(iris)
+        min_skoldpadda.circle(radie / 1.7)
+        min_skoldpadda.color(iris2)
+        min_skoldpadda.circle(radie / 2.2)
 
-for i in range(antal):
-    min_skoldpadda.forward(storlek)
-    min_skoldpadda.right(vinkel)
+def rita_pupill(radie):
+        min_skoldpadda.color(pupill)
+        min_skoldpadda.begin_fill()
+        min_skoldpadda.circle(radie / 8)
+        min_skoldpadda.end_fill()
+
+for i in range(50):
+    rita_lob(100)
+    min_skoldpadda.right(36)
+
+    min_skoldpadda.right(vinkel + 36)
+
+for i in range(100):
+    rita_oga(100)
+    min_skoldpadda.right(36)
+
+    min_skoldpadda.right(vinkel + 36)
+
+for i in range(100):
+    rita_pupill(100)
+    min_skoldpadda.right(36)
+
+    min_skoldpadda.right(vinkel + 36)
+
+
 turtle.done()
-    
-
-
-#def rita_cirkel(radie):
-#        min_skoldpadda.color("green")
-#        min_skoldpadda.right(36)
-#        min_skoldpadda.color("red")
- #       min_skoldpadda.circle(radie / 4)
-  #      min_skoldpadda.color("blue")
-   #     min_skoldpadda.circle(radie / 2)
-
-#rita_cirkel(100)
-#turtle.done()
-
 
