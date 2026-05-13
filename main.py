e429a720aacb6425ab1e26954913ac39120c65ba
Elias S <elias.sangberg@skola.taby.se>
2026-05-13 12:32:26 +0200

    Färdig med ögonsimulatorn

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import turtle
 
 
-
 iris = input("Vad är din primära ögonfärg?")
 iris2 = input("Vad är din sekundära ögonfärg?")
 
 screen = turtle.Screen()
 screen.bgcolor("#000000")
+screen.tracer(0)
 
 pupill = "black"
 
@@ -18,7 +18,7 @@ min_skoldpadda = turtle.Turtle()
 
 min_skoldpadda.shape("turtle")
 
-min_skoldpadda.speed(0)
+            
 
 def rita_lob(radie):
         min_skoldpadda.color(lob)
@@ -27,36 +27,48 @@ def rita_lob(radie):
         min_skoldpadda.end_fill()
 
 
-def rita_oga(radie):
+def rita_iris(radie):
         min_skoldpadda.color(iris)
+        min_skoldpadda.begin_fill()
         min_skoldpadda.circle(radie / 1.7)
+        min_skoldpadda.end_fill()
+
+def rita_iris2(radie):
         min_skoldpadda.color(iris2)
-        min_skoldpadda.circle(radie / 2.2)
+        min_skoldpadda.circle(radie / 1.7)
+
+
 
 def rita_pupill(radie):
         min_skoldpadda.color(pupill)
         min_skoldpadda.begin_fill()
-        min_skoldpadda.circle(radie / 8)
+        min_skoldpadda.circle(radie / 6)
         min_skoldpadda.end_fill()
 
-for i in range(50):
+for i in range(80):
     rita_lob(100)
     min_skoldpadda.right(36)
 
     min_skoldpadda.right(vinkel + 36)
 
-for i in range(100):
-    rita_oga(100)
+for i in range(80):
+    rita_iris(100)
     min_skoldpadda.right(36)
 
     min_skoldpadda.right(vinkel + 36)
 
-for i in range(100):
+for i in range(80):
+    rita_iris2(100)
+    min_skoldpadda.right(36)
+
+    min_skoldpadda.right(vinkel + 36)
+
+for i in range(80):
     rita_pupill(100)
     min_skoldpadda.right(36)
 
     min_skoldpadda.right(vinkel + 36)
 
-
 turtle.done()
+
 
